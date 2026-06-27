80db157834f15f2ce321da82aacfb557d5a4089f
damstrivandrum <aajalms@gmail.com>
2026-06-27 08:46:08 +0530

    Update exam start time to 09:00 IST

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ window.MARKS_WRONG = -1;
 window.EXAM_TZ = "Asia/Kolkata";
 
 // ✅ Fixed window (IST)
-window.EXAM_START_IST = "2026-06-27T08:00:00+05:30";
+window.EXAM_START_IST = "2026-06-27T09:00:00+05:30";
 window.EXAM_END_IST   = "2026-06-27T12:15:00+05:30";
 
 // Parsed milliseconds (used by pages)
